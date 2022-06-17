6a747fcb227483384b0c9362a6f9ed905de136ce
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2022-06-17 19:47:58 +0800

    Update event.ts

--- src/event.ts
+++ src/event.ts
@@ -1,20 +1,22 @@
 let Global: {
-  events: { [k: string]: ((...item: any) => Promise<any>)[] };
+    events: { [k: string]: ((...item: any) => Promise<any>)[] };
 } = {
-  events: {},
-};
-export function packEvent(eventName: string): (cb: () => Promise<any>) => any {
-  return function (cb: () => Promise<any>): any {
-    if (Array.isArray(Global.events[eventName]))
-      Global.events[eventName].push(cb);
-    else Global.events[eventName] = [cb];
-  };
+    events: {}
+}
+export function packEvent(eventName: string): (cb: (...item: any) => Promise<any>) => any {
+    return function(cb: (...item: any) => Promise<any>): any {
+        if (Array.isArray(Global.events[eventName]))
+            Global.events[eventName].push(cb);
+        else
+            Global.events[eventName] = [cb];
+    };
 }
 
 export async function callEvent(eventName: string, data: any): Promise<void> {
-  if (Global.events[eventName.toLowerCase()] === undefined) return;
+    if (Global.events[eventName.toLowerCase()] === undefined)
+        return;
 
-  for (const cb of Global.events[eventName.toLowerCase()]) {
-    await cb(Object.freeze(data));
-  }
+    for (const cb of Global.events[eventName.toLowerCase()]) {
+        await cb(Object.freeze(data));
+    }
 }
