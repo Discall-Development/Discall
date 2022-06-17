a257404fb2259393cc99c08c8848026464943be0
YueYue <76854136+ItzMiracleOwO@users.noreply.github.com>
2022-06-17 19:51:52 +0800

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -1,37 +1,39 @@
 import {
-  allIntents,
-  createBot,
-  GuildCreateEventData,
-  onGuildCreate,
-  onMessageCreate,
-  onReady,
-  ReadyEventData,
-} from "../src";
+    allIntents,
+    connectChannel,
+    createBot,
+    GuildCreateEventData,
+    onGuildCreate,
+    onReady,
+    ReadyEventData
+} from '../src';
 
 export async function BotTest(): Promise<void> {
-  createBot(process.env["WS_TOKEN"] as string, {
-    intents: allIntents(),
-    prefix: "!",
-  });
+    createBot(process.env['WS_TOKEN'] as string, {
+        intents: allIntents(),
+        prefix: '!'
+    });
 
-  onReady(async (data: ReadyEventData) => {
-    console.log(`API Version: ${data.v}`);
-    console.log(`Login with '${data.user.username}'`);
-  });
+    onReady(async (data: ReadyEventData) => {
+        console.log(`API Version: ${data.v}`);
+        console.log(`Login with '${data.user.username}'`);
+    });
 
-  onGuildCreate(async (data: GuildCreateEventData) => {
-    console.log(`Guild ${data.name} created`);
-  });
+    onGuildCreate(async (data: GuildCreateEventData) => {
+        console.log(`Guild ${data.name} created`);
+        if (data.id === 757188229651890186n)
+            await connectChannel(data.id, 761424295528235008n, false, true);
+    });
 
-  // onInviteCreate(async (data: any) => {
-  //     console.log(data);
-  // });
+    // onInviteCreate(async (data: any) => {
+    //     console.log(data);
+    // });
 
-  onMessageCreate(async (data: any) => {
-    console.log(data);
-  });
+    // onMessageCreate(async (data: MessageData) => {
+    //     console.log(data);
+    // });
 
-  // onMessageUpdate(async (data: any) => {
-  //     console.log(data);
-  // })
+    // onMessageUpdate(async (data: any) => {
+    //     console.log(data);
+    // })
 }
